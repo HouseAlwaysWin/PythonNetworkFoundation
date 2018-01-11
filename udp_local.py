c0e39771e2c41a8ca6e6c673f92344d798941b05
Martin <martinwang7963@gmail.com>
2018-01-11 13:47:41 +0800

    Fix udp_local.py

--- udp_local.py
+++ udp_local.py
@@ -5,8 +5,8 @@ MAX_BYTES = 65535
 
 def server(ip,port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(ip, port)
-    print('Listening at {0}'.format(sock.getsockname(())))
+    sock.bind((ip, port))
+    print('Listening at {0}'.format(sock.getsockname()))
     while True:
         data, address = sock.recvfrom(MAX_BYTES)
         text = data.decode('utf-8')
@@ -18,19 +18,19 @@ def client(ip,port):
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     text = 'The time is {0}'.format(datetime.now())
     data = text.encode('utf-8')
-    sock.sendto(data,ip,port)
+    sock.sendto(data,(ip,port))
     print('The OS assigned me the address {}'.format(sock.getsockname()))
     data,address = sock.recvfrom(MAX_BYTES)
     text = data.decode('utf-8')
-    print('The server {0} replied {2}'.format(address,text))
+    print('The server {0} replied {1}'.format(address,text))
 
 if __name__ == '__main__':
     choices = {'client': client,'server':server}
     parser = argparse.ArgumentParser(description="Send and receive UDP locally")
     parser.add_argument('role', choices=choices, help='which role to play')
-    parser.add_argument('-p', metavar='PORT',type=int,default=1060)
-    parser.add_argument('-i',metavar='IP',type=str,default='127.0.0.1')
+    parser.add_argument('-p', metavar='port',type=int,default=1060,help='UDP Port (default 1060)')
+    parser.add_argument('-i',metavar='ip',type=str,default='127.0.0.1')
     args = parser.parse_args()
     function = choices[args.role]
-    function(args.p)
+    function(args.i,args.p)
 
