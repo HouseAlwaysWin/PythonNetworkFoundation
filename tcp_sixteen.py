30a6d01d6b1730ad69533b9f68191f84ea5f4b8a
Martin <martinwang7963@gmail.com>
2018-05-13 15:57:43 +0800

    fix code

--- tcp_sixteen.py
+++ tcp_sixteen.py
@@ -5,18 +5,19 @@ import socket
 def recvall(sock, length):
     data = b''
     while len(data) < length:
-        more = ock.recv(length - len(data))
+        more = sock.recv(length - len(data))
         if not more:
             raise EOFError('was expecting %d bytes but only recived'
-                            ' %d bytes before the socket closed' % (length, len(data)))
+                           ' %d bytes before the socket closed' % (length, len(data)))
         data += more
     return data
 
 
 def server(interface, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(sockt.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((interface, port))
+    sock.listen(1)
     print('Listening at', sock.getsockname())
 
     while True:
@@ -24,9 +25,9 @@ def server(interface, port):
         print('We have accepted a connection from', sockname)
         print(' Socket name:', sc.getsockname())
         print(' Socket peer:', sc.getpeername())
-        message = recvall(sc.16)
+        message = recvall(sc, 16)
         print(' Incoming sixteen-octet message:', repr(message))
-        sc.sendall(b'Farewell, client)
+        sc.sendall(b'Farewell, client')
         sc.close()
         print(' Reply sent, socket closed')
 
@@ -47,3 +48,8 @@ if __name__ == '__main__':
     parser.add_argument('role', choices=choices, help='which role to play')
     parser.add_argument('host', help='interface the server listens at:'
                         'host the client sends to')
+    parser.add_argument('-p', metavar='PORT', type=int,
+                         default=1060, help='TCP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.host, args.p)
