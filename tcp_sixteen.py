23909a7e2a298d263c41652033951d8b2e7d8dda
Martin <martinwang7963@gmail.com>
2018-05-13 08:47:15 +0800

    add tcp server and client method

--- tcp_sixteen.py
+++ tcp_sixteen.py
@@ -1,14 +1,49 @@
-import argparse,socket
+import argparse
+import socket
 
-def recvall(sock,length):
+
+def recvall(sock, length):
     data = b''
     while len(data) < length:
         more = ock.recv(length - len(data))
         if not more:
             raise EOFError('was expecting %d bytes but only recived'
-            ' %d bytes before the socket closed' % (length,len(data)))
+                            ' %d bytes before the socket closed' % (length, len(data)))
         data += more
     return data
 
-def server(interface,port):
-             
+
+def server(interface, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(sockt.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((interface, port))
+    print('Listening at', sock.getsockname())
+
+    while True:
+        sc, sockname = sock.accept()
+        print('We have accepted a connection from', sockname)
+        print(' Socket name:', sc.getsockname())
+        print(' Socket peer:', sc.getpeername())
+        message = recvall(sc.16)
+        print(' Incoming sixteen-octet message:', repr(message))
+        sc.sendall(b'Farewell, client)
+        sc.close()
+        print(' Reply sent, socket closed')
+
+
+def client(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect((host, port))
+    print('Client has been assigned socket name', sock.getsockname())
+    reply = recvall(sock, 16)
+    print('The server said', repr(reply))
+    sock.sendall(b'Hi there server')
+    sock.close()
+
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send and receive over TCP')
+    parser.add_argument('role', choices=choices, help='which role to play')
+    parser.add_argument('host', help='interface the server listens at:'
+                        'host the client sends to')
