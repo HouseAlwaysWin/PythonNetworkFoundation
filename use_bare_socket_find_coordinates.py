b90ac8f74e1f9fbc5799d58d8572cfda52ec8ed3
Martin <martinwang7963@gmail.com>
2018-05-14 09:23:31 +0800

    add example

--- use_bare_socket_find_coordinates.py
+++ use_bare_socket_find_coordinates.py
@@ -25,6 +25,62 @@ def geocode(address):
     print(raw_reply.decode('utf-8'))
 
 
+def geocode(address):
+    sock = socket.socket()
+    sock.connect(('maps.google.com', 80))
+    request = request_text.format(quote_plus(address))
+    sock.sendall(request.encode('ascii'))
+    raw_reply = b''
+    while True:
+        more = sock.recv(4096)
+        if not more:
+            break
+        raw_reply += more
+    print(raw_reply.decode('utf-8'))
+
+
+def geocode(address):
+    sock = socket.socket()
+    sock.connect(('maps.google.com', 80))
+    request = request_text.format(quote_plus(address))
+    sock.sendall(request.encode('ascii'))
+    raw_reply = b''
+    while True:
+        more = sock.recv(4096)
+        if not more:
+            break
+        raw_reply += more
+    print(raw_reply.decode('utf-8'))
+
+
+def geocode(address):
+    sock = socket.socket()
+    sock.connect(('maps.google.com', 80))
+    request = request_text.format(quote_plus(address))
+    sock.sendall(request.encode('ascii'))
+    raw_reply = b''
+    while True:
+        more = sock.recv(4096)
+        if not more:
+            break
+        raw_reply += more
+    print(raw_reply.decode('utf-8'))
+
+
+def geocode(address):
+    sock = socket.socket()
+    sock.connect(('maps.google.com', 80))
+    request = request_text.format(quote_plus(address))
+    sock.sendall(request.encode('ascii'))
+    raw_reply = b''
+    while True:
+        more = sock.recv(4096)
+        if not more:
+            break
+        raw_reply += more
+    print(raw_reply.decode('utf-8'))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Use socket connect google map')
     parser.add_argument('-a', metavar='address', type=str, default='207 N. Defiance St, Archbold, OH')
